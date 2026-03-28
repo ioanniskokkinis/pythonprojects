aac8b611bea259ce75098bedff130ebd7356b755
Ioannis Kokkinis <ioanniskokkinis@proton.me>
2026-03-28 17:12:32 +0200

    Add basic Hangman game structure
    Initial implementation of a Hangman game with word selection and input handling.

---
+++ hangman/main.py
@@ -0,0 +1,20 @@
+import random
+import main
+
+WORDS = ("python" , "jumble" , "easy" , "difficult" , "answer" , "xylophene")
+random = random.choise(WORDS)
+attemps = 6
+end_of_game = false
+da = input("Hello!You are starting the game.Press y to start")
+if(da == "y" and end_of_game):
+    display = 
+    for _ in da:
+        display += _
+    random_keyword = random.replace('a' , '_')
+    char = input("Give me the character")
+    for i in da:
+        if(char == da):
+            attemps = attemps - 1;
+
+
+attemps = attemps - 1
